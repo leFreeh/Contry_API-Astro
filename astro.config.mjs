aec88e688da50e07937eccc28e1c79a0b76c5180
Dennys Wong <freewheel1998@hotmail.com>
2024-12-04 00:59:01 +0100

    Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from '@astro/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  adapter: vercel(),
 });
